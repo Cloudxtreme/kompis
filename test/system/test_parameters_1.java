b52b793d0cbbb7916a577f050f0462bff1fe2a22
Mikael Auno <auno@kth.se>
2009-07-30 17:31:37 +0200

    Added tests for order of lists.

---
+++ test/system/test_parameters_1.java
@@ -0,0 +1,14 @@
+class Main
+{
+    public static void main(String[] s)
+    {
+        System.out.println(1);
+    }
+}
+
+class A {
+    public int f(int x, int y)
+    {
+        return 0;	
+    }
+}
---
+++ test/system/test_variables_1.java
@@ -0,0 +1,16 @@
+class Main
+{
+    public static void main(String[] s)
+    {
+        System.out.println(1);
+    }
+}
+
+class A {
+    public int f(int x)
+    {
+        int y;
+        int z;
+        return 0;	
+    }
+}
