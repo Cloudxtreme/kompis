04c18980edeb820dc5bb0cddd7ce38bb35cf17c5
Jesper Särnesjö <jesper@sarnesjo.org>
2009-07-30 03:01:00 +0200

    Added test_class_* files.

---
+++ test/system/test_class_3.java
@@ -0,0 +1,15 @@
+class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println(1);
+  }
+}
+
+class A
+{
+  public int a()
+  {
+    return 0;
+  }
+}
---
+++ test/system/test_class_4.java
@@ -0,0 +1,17 @@
+class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println(1);
+  }
+}
+
+class A
+{
+  int i;
+
+  public int a()
+  {
+    return 0;
+  }
+}
