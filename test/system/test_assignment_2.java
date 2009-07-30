4e8fdd8e204a13891440661bef2b18b730ea1edd
Jesper Särnesjö <jesper@sarnesjo.org>
2009-07-30 15:48:04 +0200

    Added test_assignment_2.* files.

---
+++ test/system/test_assignment_2.java
@@ -0,0 +1,11 @@
+class Main
+{
+  public static void main(String[] args)
+  {
+    // main may only have one statement, so we use a statement list
+    {
+      // this is valid syntactically, but not semantically
+      i = 0;
+    }
+  }
+}
