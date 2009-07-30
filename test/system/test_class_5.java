ecc139e129f1e83bf0096a03698ce92a3528b2bd
Jesper Särnesjö <jesper@sarnesjo.org>
2009-07-30 17:41:55 +0200

    Added a system test for method declaration lists.

---
+++ test/system/test_class_5.java
@@ -0,0 +1,20 @@
+class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println(1);
+  }
+}
+
+class A
+{
+  public int a()
+  {
+    return 0;
+  }
+
+  public int b()
+  {
+    return 0;
+  }
+}
