e95218708ae16e40bfd8d24399b18d319d410150
Jesper Särnesjö <jesper@sarnesjo.org>
2009-08-03 16:29:13 +0200

    Changed system test script to compare output with *.ast and *.err files if available.

--- test/system/_test_all.sh
+++ test/system/_test_all.sh
@@ -10,9 +10,22 @@ popd
 # find all files matching test*.java, run them through compiler, compare output with expected
 for INPUT_FILE in $(ls test*.java); do
   echo testing $INPUT_FILE...
-  ../../src/kompis <$INPUT_FILE >${INPUT_FILE/.java/.out}
-  diff -u ${INPUT_FILE/.java/.ast} ${INPUT_FILE/.java/.out}
-  rm ${INPUT_FILE/.java/.out}
+
+  # run kompis, put stdout in *.astout and stderr in *.errout
+  ../../src/kompis <$INPUT_FILE >${INPUT_FILE/.java/.astout} 2>${INPUT_FILE/.java/.errout}
+
+  # if a *.ast file with expected stdout output exists, diff it with *.astout
+  if [ -f ${INPUT_FILE/.java/.ast} ]; then
+    diff -u ${INPUT_FILE/.java/.ast} ${INPUT_FILE/.java/.astout}
+  fi
+
+  # if a *.err file with expected stderr output exists, diff it with *.errout
+  if [ -f ${INPUT_FILE/.java/.err} ]; then
+    diff -u ${INPUT_FILE/.java/.err} ${INPUT_FILE/.java/.errout}
+  fi
+
+  # remove output files
+  rm ${INPUT_FILE/.java/.astout} ${INPUT_FILE/.java/.errout}
 done
 
 # remove build products
