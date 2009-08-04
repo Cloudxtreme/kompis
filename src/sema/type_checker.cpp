1f90483d85fdeed3cb8e29ccb582d22b357d13ad
Jesper Särnesjö <jesper@sarnesjo.org>
2009-08-04 12:34:04 +0200

    Improved type checker. It probably does everything it can do without a symbol table now.

--- src/sema/type_checker.cpp
+++ src/sema/type_checker.cpp
@@ -10,35 +10,75 @@ namespace kompis
   {
     TypeData *TypeChecker::visit(AdditionExpression *x)
     {
-      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
-      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       type::Type t = type::T_INT;
+
+      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
       if(l->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "lhs of '+' not int");
         t = type::T_ERROR;
       }
+
+      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       if(r->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "rhs of '+' not int");
         t = type::T_ERROR;
       }
+
       return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(ArrayAssignmentStatement *x)
     {
+      TypeData *a = static_cast<TypeData *>(x->_id->accept(this));
+      if(a->_type != type::T_INT_ARRAY)
+        error("", x->_line_num, "type", "lhs of '[]=' not int array");
+
+      TypeData *i = static_cast<TypeData *>(x->_index->accept(this));
+      if(i->_type != type::T_INT)
+        error("", x->_line_num, "type", "array index not int");
+
+      TypeData *v = static_cast<TypeData *>(x->_value->accept(this));
+      if(v->_type != type::T_INT)
+        error("", x->_line_num, "type", "rhs of '[]=' not int");
+
       return NULL;
     }
 
     TypeData *TypeChecker::visit(ArrayLengthExpression *x)
     {
-      return new TypeData(type::T_INT);
+      type::Type t = type::T_INT;
+
+      TypeData *a = static_cast<TypeData *>(x->_array->accept(this));
+      if(a->_type != type::T_INT_ARRAY)
+      {
+        error("", x->_line_num, "type", "self of 'length' not int array"); // TODO: better error message?
+        t = type::T_ERROR;
+      }
+
+      return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(ArraySubscriptExpression *x)
     {
-      return new TypeData(type::T_INT);
+      type::Type t = type::T_INT;
+
+      TypeData *a = static_cast<TypeData *>(x->_array->accept(this));
+      if(a->_type != type::T_INT_ARRAY)
+      {
+        error("", x->_line_num, "type", "self of 'length' not int array"); // TODO: better error message?
+        t = type::T_ERROR;
+      }
+
+      TypeData *i = static_cast<TypeData *>(x->_index->accept(this));
+      if(i->_type != type::T_INT)
+      {
+        error("", x->_line_num, "type", "array index not int");
+        t = type::T_ERROR;
+      }
+
+      return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(AssignmentStatement *x)
@@ -68,11 +108,19 @@ namespace kompis
 
     TypeData *TypeChecker::visit(CallExpression *x)
     {
-      // TODO
-      x->_self->accept(this);
-      x->_method_name->accept(this);
-      x->_args->accept(this);
-      return new TypeData(type::T_INT);
+      type::Type t = type::T_INT; // TODO: use return type from method declaration
+
+      TypeData *s = static_cast<TypeData *>(x->_self->accept(this));
+      if(s->_type != type::T_OBJECT)
+      {
+        error("", x->_line_num, "type", "self of 'call' not object"); // TODO: better error message?
+        t = type::T_ERROR;
+      }
+
+      x->_method_name->accept(this); // TODO: check defined
+      x->_args->accept(this);        // TODO: check each arg against method declaration
+
+      return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(ClassDeclaration *x)
@@ -92,19 +140,22 @@ namespace kompis
 
     TypeData *TypeChecker::visit(ConjunctionExpression *x)
     {
-      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
-      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       type::Type t = type::T_BOOLEAN;
+
+      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
       if(l->_type != type::T_BOOLEAN)
       {
         error("", x->_line_num, "type", "lhs of '&&' not boolean");
         t = type::T_ERROR;
       }
+
+      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       if(r->_type != type::T_BOOLEAN)
       {
         error("", x->_line_num, "type", "rhs of '&&' not boolean");
         t = type::T_ERROR;
       }
+
       return new TypeData(t);
     }
 
@@ -127,15 +178,18 @@ namespace kompis
 
     TypeData *TypeChecker::visit(IdentifierType *x)
     {
-      return new TypeData(type::T_INT); // TODO
+      return NULL;
     }
 
     TypeData *TypeChecker::visit(IfThenElseStatement *x)
     {
-      // TODO
-      x->_pred->accept(this);
+      TypeData *p = static_cast<TypeData *>(x->_pred->accept(this));
+      if(p->_type != type::T_BOOLEAN)
+        error("", x->_line_num, "type", "argument to 'if' not boolean");
+
       x->_then_statement->accept(this);
       x->_else_statement->accept(this);
+
       return NULL;
     }
 
@@ -156,19 +210,22 @@ namespace kompis
 
     TypeData *TypeChecker::visit(LessThanExpression *x)
     {
-      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
-      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       type::Type t = type::T_BOOLEAN;
+
+      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
       if(l->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "lhs of '<' not int");
         t = type::T_ERROR;
       }
+
+      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       if(r->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "rhs of '<' not int");
         t = type::T_ERROR;
       }
+
       return new TypeData(t);
     }
 
@@ -180,12 +237,12 @@ namespace kompis
 
     TypeData *TypeChecker::visit(MethodDeclaration *x)
     {
-      x->_return_type->accept(this);
+      x->_return_type->accept(this); // TODO: check defined
       x->_name->accept(this);
       x->_params->accept(this);
       x->_vars->accept(this);
       x->_statements->accept(this);
-      x->_return_expr->accept(this);
+      x->_return_expr->accept(this); // TODO: check same as return type
       return NULL;
     }
 
@@ -198,49 +255,72 @@ namespace kompis
 
     TypeData *TypeChecker::visit(MultiplicationExpression *x)
     {
-      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
-      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       type::Type t = type::T_INT;
+
+      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
       if(l->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "lhs of '*' not int");
         t = type::T_ERROR;
       }
+
+      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       if(r->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "rhs of '*' not int");
         t = type::T_ERROR;
       }
+
       return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(NegationExpression *x)
     {
-      TypeData *r = static_cast<TypeData *>(x->_expr->accept(this));
       type::Type t = type::T_BOOLEAN;
+
+      TypeData *r = static_cast<TypeData *>(x->_expr->accept(this));
       if(r->_type != type::T_BOOLEAN)
       {
         error("", x->_line_num, "type", "rhs of '!' not boolean");
         t = type::T_ERROR;
       }
+
       return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(NewIntegerArrayExpression *x)
     {
-      return new TypeData(type::T_INT_ARRAY);
+      type::Type t = type::T_INT_ARRAY;
+
+      TypeData *c = static_cast<TypeData *>(x->_count->accept(this));
+      if(c->_type != type::T_INT)
+      {
+        error("", x->_line_num, "type", "argument to 'new[]' not int");
+        t = type::T_ERROR;
+      }
+
+      return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(NewObjectExpression *x)
     {
-      // TODO
-      x->_class_name->accept(this);
-      return new TypeData(type::T_OBJECT);
+      // TODO: correct behavior?
+
+      type::Type t = type::T_OBJECT;
+
+      TypeData *c = static_cast<TypeData *>(x->_class_name->accept(this));
+      if(c->_type != type::T_OBJECT)
+      {
+        error("", x->_line_num, "type", "argument to 'new' not object");
+        t = type::T_ERROR;
+      }
+
+      return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(ParameterDeclaration *x)
     {
-      x->_type->accept(this);
+      x->_type->accept(this); // TODO: check defined
       x->_name->accept(this);
       return NULL;
     }
@@ -276,30 +356,34 @@ namespace kompis
 
     TypeData *TypeChecker::visit(SubtractionExpression *x)
     {
-      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
-      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       type::Type t = type::T_INT;
+
+      TypeData *l = static_cast<TypeData *>(x->_left->accept(this));
       if(l->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "lhs of '-' not int");
         t = type::T_ERROR;
       }
+
+      TypeData *r = static_cast<TypeData *>(x->_right->accept(this));
       if(r->_type != type::T_INT)
       {
         error("", x->_line_num, "type", "rhs of '-' not int");
         t = type::T_ERROR;
       }
+
       return new TypeData(t);
     }
 
     TypeData *TypeChecker::visit(ThisExpression *x)
     {
-      return NULL;
+      // TODO: correct behavior?
+      return new TypeData(type::T_OBJECT);
     }
 
     TypeData *TypeChecker::visit(VariableDeclaration *x)
     {
-      x->_type->accept(this);
+      x->_type->accept(this); // TODO: check defined
       x->_name->accept(this);
       return NULL;
     }
@@ -313,9 +397,12 @@ namespace kompis
 
     TypeData *TypeChecker::visit(WhileDoStatement *x)
     {
-      // TODO
-      x->_pred->accept(this);
+      TypeData *p = static_cast<TypeData *>(x->_pred->accept(this));
+      if(p->_type != type::T_BOOLEAN)
+        error("", x->_line_num, "type", "argument to 'while' not boolean");
+
       x->_s->accept(this);
+
       return NULL;
     }
   }
