c0ff86806a69832bf196f067b26882ceeab95481
Jesper Särnesjö <jesper@sarnesjo.org>
2009-08-04 16:01:07 +0200

    Changed system test script to use make.

--- test/system/_test_all.sh
+++ test/system/_test_all.sh
@@ -3,8 +3,8 @@
 pushd $(dirname $0)
 
 # build compiler
-pushd ../../src
-flex lexer.l && bison -d parser.ypp && g++ -Wall -I. -o kompis lex.yy.c parser.tab.cpp ast/*.cpp sema/*.cpp
+pushd ../../
+make
 popd
 
 # find all files matching test*.java, run them through compiler, compare output with expected
@@ -18,7 +18,7 @@ for INPUT_FILE in $(ls test*.java); do
   [ -f $ERR_FILE ] && ERROUT_FILE=${INPUT_FILE/.java/.errout} || ERROUT_FILE="/dev/null"
 
   # run kompis
-  ../../src/kompis <$INPUT_FILE >$ASTOUT_FILE 2>$ERROUT_FILE
+  ../../bin/kompis <$INPUT_FILE >$ASTOUT_FILE 2>$ERROUT_FILE
 
   # if a *.ast file with expected stdout output exists, diff it with *.astout
   # remove output file only if there was no diff
@@ -28,10 +28,5 @@ for INPUT_FILE in $(ls test*.java); do
   # remove output file only if there was no diff
   [ -f $ERR_FILE ] && diff -q $ERR_FILE $ERROUT_FILE && rm $ERROUT_FILE
 done
-
-# remove build products
-pushd ../../src
-rm -f kompis lex.yy.c parser.tab.cpp parser.tab.hpp
-popd
 
 popd
