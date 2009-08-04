c3cc4fc65040b3bd3d3d1ba4919d039048774413
Jesper Särnesjö <jesper@sarnesjo.org>
2009-08-04 21:37:22 +0200

    Removed unit tests.

--- test/unit/_test_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# find all files matching test*.sh and run them
-for TEST_FILE in $(ls test*.sh); do
-  echo running $TEST_FILE...
-  . $TEST_FILE
-done
--- test/unit/test_ast_pp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./cxxtest/cxxtestgen.pl --error-printer -o test.cpp test_ast_pp.hpp && g++ -Wall -I./cxxtest -I../../src -o test test.cpp ../../src/ast/*.cpp && ./test
-rm -f test test.cpp
